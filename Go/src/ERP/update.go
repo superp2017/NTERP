db78c9952146ddeec07eb111ba221dec2f237554
mapeng <mapeng@fastcdn.com>
2018-12-04 19:04:02 +0800

    update

--- Go/src/ERP/update.go
+++ Go/src/ERP/update.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"JGo/JUtil"
 	"sync"
 )
 
@@ -11,32 +12,67 @@ type Cache struct {
 }
 
 type DataCache struct {
-	Data      map[string]*Cache //具体数据
-	StampID   map[int64]string  //时间戳和ID的映射
-	IDStamp   map[string]int64  //ID和时间戳的映射
-	Stamps    []int64           //时间戳的列表
-	LastStamp int64             //最后更新的时间
-	Mutex     sync.Mutex        //数据锁
+	Data      map[string]interface{} //具体数据
+	StampID   map[int64]string       //时间戳和ID的映射
+	IDStamp   map[string]int64       //ID和时间戳的映射
+	Stamps    []int64                //时间戳的列表
+	LastStamp int64                  //最后更新的时间
+	Mutex     sync.Mutex             //数据锁
+	Timer     *JUtil.Timer           //定时删除
+}
 
+func (da *DataCache) timeout() {
+	da.Mutex.Lock()
+	defer da.Mutex.Unlock()
+	stamp := CurStamp()
+	index := -1
+	for i, t := range da.Stamps {
+		if stamp-t >= 3600 {
+			if id, ok := da.StampID[t]; ok {
+				delete(da.IDStamp, id)
+				delete(da.Data, id)
+			}
+			delete(da.StampID, t)
+			if index == -1 {
+				index = i
+			}
+		}
+	}
+	da.Stamps = da.Stamps[:index]
 }
 
 func (da *DataCache) updateCache(id string, data interface{}) {
 	da.Mutex.Lock()
 	defer da.Mutex.Unlock()
 	da.LastStamp = CurStamp()
+	da.Stamps = append(da.Stamps, da.LastStamp)
 	if _, ok := da.Data[id]; ok {
-		delete(da.StampID, da.Data[id].TimeStamp)
-		da.Data[id].TimeStamp = da.LastStamp
-		da.Stamps = append(da.Stamps, da.LastStamp)
+		if t, o := da.IDStamp[id]; o {
+			delete(da.StampID, t)
+		}
+		da.StampID[da.LastStamp] = id
+	} else {
+		da.Data[id] = data
+		da.IDStamp[id] = da.LastStamp
 		da.StampID[da.LastStamp] = id
 	}
 }
 
-func (da *DataCache) modCache(data interface{}) {
+func (da *DataCache) getData(stamp int64) []interface{} {
+	if stamp > da.LastStamp {
+		return []interface{}{}
+	}
+	var res []interface{}
 	da.Mutex.Lock()
 	defer da.Mutex.Unlock()
-	da.LastStamp = CurStamp()
-	//if _,ok:=[]
+	for _, t := range da.Stamps {
+		if id, ok := da.StampID[t]; ok {
+			if d, o := da.Data[id]; o {
+				res = append(res, d)
+			}
+		}
+	}
+	return res
 }
 
 func init() {
